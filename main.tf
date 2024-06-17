6b39af54d62ab9355977f84817b973c2cd5e463b
Chris Totten <christotten@Chriss-MacBook-Air.local>
2024-06-17 17:06:51 +0100

    Added new vnet2

--- main.tf
+++ main.tf
@@ -39,3 +39,11 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "uksouth"
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+# Create a second virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet2"
+  address_space       = ["10.0.0.1/16"]
+  location            = "uksouth"
+  resource_group_name = azurerm_resource_group.rg.name
+}
